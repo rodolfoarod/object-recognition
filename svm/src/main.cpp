c8c42e062b0f1b02ea46831d7da05e23b13fccb2
Rodolfo Rodrigues <rodolfoarod@gmail.com>
2016-12-19 17:03:37 +0000

    SVM - Initial setup

---
+++ svm/src/main.cpp
@@ -0,0 +1,14 @@
+// ============================
+// MAIN.CPP
+// ============================
+
+#include <iostream>
+
+#include <opencv2/opencv.hpp>
+
+// Main Function
+int main(int argc, const char* argv[]) {
+
+    std::cout << "OpenCV Version: " << CV_VERSION << std::endl;
+    
+}
